f2469e74398a8a96602a29d73ea1c75a09f057ab
lfernando316 <luiz_fernando316@hotmail.com>
2022-12-09 16:23:48 -0500

    creacion de crud con encriptacion de contraseñas y rutas protejidas

---
+++ src/users/users.router.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const JwtPassport = require("../middleware/auth.middleware");
+
+const userServices = require("./users.services");
+
+router.get(
+  "/",
+  JwtPassport.authenticate("jwt", { session: false }),
+  userServices.getAllUsers
+); //? /api/v1/users
+router.post("/", userServices.postUser); //? /api/v1/users
+
+router.get("/:id", userServices.getUserById); //? /api/v1/users/:id
+router.patch(
+  "/:id",
+  JwtPassport.authenticate("jwt", { session: false }),
+  userServices.patchUser
+); //? /api/v1/users/:id
+router.delete(
+  "/:id",
+  JwtPassport.authenticate("jwt", { session: false }),
+  userServices.deleteUser
+); //? /api/v1/users/:id
+
+module.exports = router;
